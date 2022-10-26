31cc359fa3e1edafe903683424357d513a0576f1
RaThida <100819767+RaThida@users.noreply.github.com>
2022-10-26 16:43:58 +0700

    admin folder

---
+++ login.php
@@ -0,0 +1,83 @@
+<?php require_once('../config.php') ?>
+<!DOCTYPE html>
+<html lang="en" class="" style="height: auto;">
+ <?php //require_once('inc/header.php') ?>
+<body class="hold-transition login-page ">
+<link rel="stylesheet" href="<?php echo base_url ?>assets/css/login.css">
+  <script>
+    start_loader()
+  </script>
+  <div class="container mb-4">
+    <h2 class="text-center"><?php echo $_settings->info('name') ?></h2>
+   
+  </div>
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="./" class="h1"><b>Login</b></a>
+    </div>
+    <div class="card-body">
+      <form id="login-frm" action="" method="post">
+          
+          <label>Username:</label>
+          <div class="input-group mb-3">                        
+                                                  
+             <input type="username" class="form-control" name="username" placeholder="username">                                            
+            <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-user"></span>
+                  </div>
+            </div>          
+        </div>
+        
+        <label class="text-center">password:</label>
+        <div class="input-group mb-3 ">
+          <input type="password" class="form-control" name="password" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+
+        
+
+        <div class="row">
+          <div class="col-8">
+            <!-- <a href="<?php echo base_url ?>">Go to Website</a> -->
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <!-- /.social-auth-links -->
+
+      <!-- <p class="mb-1">
+        <a href="forgot-password.html">I forgot my password</a>
+      </p> -->
+      
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+
+<script>
+  $(document).ready(function(){
+    end_loader();
+  })
+</script>
+</body>
+</html>
