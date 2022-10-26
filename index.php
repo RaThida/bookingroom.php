e862cac6b748d8f23f44bd8d6ee5059467469afc
RaThida <100819767+RaThida@users.noreply.github.com>
2022-10-26 16:48:39 +0700

    admin_report

--- index.php
+++ index.php
@@ -1,97 +1,150 @@
 <?php 
-$user = $conn->query("SELECT * FROM users where id ='".$_settings->userdata('id')."'");
-foreach($user->fetch_array() as $k =>$v){
-	$meta[$k] = $v;
-}
+$dstart = isset($_GET['dstart']) ? $_GET['dstart'] : date("Y-m-d", strtotime(date("Y-m-d")." -1 week"));
+$dend = isset($_GET['dend']) ? $_GET['dend'] : date("Y-m-d");
+$rid = isset($_GET['aid']) ? $_GET['aid'] : 0;
 ?>
-<?php if($_settings->chk_flashdata('success')): ?>
-<script>
-	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
-</script>
-<?php endif;?>
 <div class="card card-outline card-primary">
-	<div class="card-body">
-		<div class="container-fluid">
-			<div id="msg"></div>
-			<form action="" id="manage-user">	
-				<input type="hidden" name="id" value="<?php echo $_settings->userdata('id') ?>">
-				<div class="form-group">
-					<label for="name">First Name</label>
-					<input type="text" name="firstname" id="firstname" class="form-control" value="<?php echo isset($meta['firstname']) ? $meta['firstname']: '' ?>" required>
-				</div>
-				<div class="form-group">
-					<label for="name">Last Name</label>
-					<input type="text" name="lastname" id="lastname" class="form-control" value="<?php echo isset($meta['lastname']) ? $meta['lastname']: '' ?>" required>
-				</div>
-				<div class="form-group">
-					<label for="username">Username</label>
-					<input type="text" name="username" id="username" class="form-control" value="<?php echo isset($meta['username']) ? $meta['username']: '' ?>" required  autocomplete="off">
-				</div>
-				<div class="form-group">
-					<label for="password">Password</label>
-					<input type="password" name="password" id="password" class="form-control" value="" autocomplete="off">
-					<small><i>Leave this blank if you dont want to change the password.</i></small>
-				</div>
-				<div class="form-group">
-					<label for="" class="control-label">Avatar</label>
-					<div class="custom-file">
-		              <input type="file" class="custom-file-input rounded-circle" id="customFile" name="img" onchange="displayImg(this,$(this))">
-		              <label class="custom-file-label" for="customFile">Choose file</label>
-		            </div>
-				</div>
-				<div class="form-group d-flex justify-content-center">
-					<img src="<?php echo validate_image(isset($meta['avatar']) ? $meta['avatar'] :'') ?>" alt="" id="cimg" class="img-fluid img-thumbnail">
-				</div>
-			</form>
+	<div class="card-header">
+		<h3 class="card-title">Schedule Report</h3>
+		<div class="card-tools">
+			
 		</div>
 	</div>
-	<div class="card-footer">
-			<div class="col-md-12">
-				<div class="row">
-					<button class="btn btn-sm btn-primary" form="manage-user">Update</button>
-				</div>
-			</div>
+	<div class="card-body">
+		<div class="container-fluid">
+        <form action="" id="filter">
+            <div class="d-flex  h-100 d-flex align-items-end">
+                <div class="form-group col-3">
+                    <label for="start" class="control-label">Date Start</label>
+                    <input type="date" name="start" id="start" value="<?php echo $dstart ?>" class="form-control">
+                </div>
+                <div class="form-group col-3">
+                    <label for="end" class="control-label">Date End</label>
+                    <input type="date" name="end" id="end" value="<?php echo $dend ?>" class="form-control">
+                </div>
+                <div class="form-group col-3">
+                    <label for="aid" class="control-label">Room/Hall</label>
+                    <select class="custom-select select2" name="aid" id="aid">
+                        <option value="0" <?php echo $rid ==  0 ? "selected" : "" ?>>All</option>
+                        <?php 
+                        $aqry = $conn->query("SELECT * FROM `assembly_hall` order by room_name asc");
+                        while($row= $aqry->fetch_assoc()):
+                        ?>
+                        <option value="<?php echo $row['id'] ?>" <?php echo $rid ==  $row['id']  ? "selected" : "" ?>><?php echo $row['room_name'] ?></option>
+
+                        <?php endwhile; ?>
+                    </select>
+                </div>
+                <div class="form-group col-3">
+                    <button class="btn btn-flat btn-primary"><span class="fas fa-filter"></span>  Filter</button>
+                    <button type="button" id="print_now" class="btn btn-flat btn-success"><span class="fas fa-print"></span>  Print</button>
+                </div>
+            </div>
+        </form>
+        <div class="container-fluid" id="print_out">
+			<table class="table table-bordered table-stripped" id="report-table">
+				<colgroup>
+					<col width="5%">
+					<col width="20%">
+					<col width="20%">
+					<col width="20%">
+					<col width="15%">
+					<col width="20%">
+				</colgroup>
+				<thead>
+					<tr>
+						<th class="text-center">#</th>
+						<th>Schedule</th>
+						<th>Hall/Room Name</th>
+						<th>Description</th>
+						<th>Reserved By</th>
+						<th>Remarks</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+					$i = 1;
+                        $where = "";
+                        $rname = "";
+                        if($rid > 0){
+                            $where = " and a.id = '{$rid}'";
+                        }
+						$qry = $conn->query("SELECT s.*,a.room_name, a.description from `schedule_list` s inner join `assembly_hall` a on a.id = s.assembly_hall_id where ((date(datetime_start) BETWEEN '{$dstart}' and '{$dend}' ) OR (date(datetime_end) BETWEEN '{$dstart}' and '{$dend}' )) {$where} order by unix_timestamp(datetime_start) asc, unix_timestamp(datetime_end) asc ");
+						while($row = $qry->fetch_assoc()):
+                            if($rid > 0){
+                                $rname = $row['room_name'];
+                            }
+					?>
+						<tr>
+							<td class="text-center"><?php echo $i++; ?></td>
+							<td>
+                                <p class="m-0">
+                                    <small><b>Start:</b> <?php echo date("M d, Y h:i A",strtotime($row['datetime_start'])) ?></small><br>
+                                    <small><b>End:</b> <?php echo date("M d, Y h:i A",strtotime($row['datetime_end'])) ?></small>
+                                </p>
+                            </td>
+							<td ><?php echo $row['room_name'] ?></td>
+							<td ><?php echo $row['description'] ?></td>
+							<td ><?php echo ucwords($row['reserved_by']) ?></td>
+							<td ><?php echo $row['schedule_remarks'] ?></td>
+							</td>
+						</tr>
+					<?php endwhile; ?>
+				</tbody>
+			</table>
 		</div>
+		</div>
+	</div>
 </div>
-<style>
-	img#cimg{
-		height: 15vh;
-		width: 15vh;
-		object-fit: cover;
-		border-radius: 100% 100%;
-	}
-</style>
 <script>
-	function displayImg(input,_this) {
-	    if (input.files && input.files[0]) {
-	        var reader = new FileReader();
-	        reader.onload = function (e) {
-	        	$('#cimg').attr('src', e.target.result);
-	        }
-
-	        reader.readAsDataURL(input.files[0]);
-	    }
-	}
-	$('#manage-user').submit(function(e){
-		e.preventDefault();
-		start_loader()
-		$.ajax({
-			url:_base_url_+'classes/Users.php?f=save',
-			data: new FormData($(this)[0]),
-		    cache: false,
-		    contentType: false,
-		    processData: false,
-		    method: 'POST',
-		    type: 'POST',
-			success:function(resp){
-				if(resp ==1){
-					location.reload()
-				}else{
-					$('#msg').html('<div class="alert alert-danger">Username already exist</div>')
-					end_loader()
-				}
-			}
-		})
-	})
-
+    window.DT_INIT = function(){
+        $('#report-table').dataTable({
+            "lengthMenu":[ [ 50, 100, -1], [ 50, 100, "All"]],
+            "oreder": [[0,"asc"]]
+        })
+    }
+    $(function(){
+        $('#filter').submit(function(e){
+            e.preventDefault()
+            var dstart = $('[name="start"]').val()
+            var dend = $('[name="end"]').val()
+            var aid = $('[name="aid"]').val()
+            location.href = "?page=report&dstart="+dstart+"&dend="+dend+"&aid="+aid;
+        })
+        $('.select2').select2()
+        DT_INIT()
+        // console.log($.fn.DataTable.isDataTable( '#report-table' ))
+        $('#print_now').click(function(){
+            start_loader()
+            if($.fn.DataTable.isDataTable( '#report-table' ) == true){
+                $('#report-table').DataTable().destroy()
+            }
+            var _h = $('head').clone()
+            var _p = $('#print_out').clone()
+            var _el = $('<div>')
+                _el.append(_h)
+                _el.append("<style>html, body, .wrapper {  min-height: inherit !important; }</style>")
+                _el.append("<h3 class='text-center'><?php echo $_settings->info('name') ?></h3>")
+                _el.append("<h4 class='text-center'>Schedule Report</h4>")
+                if('<?php echo $dstart ?>' == '<?php echo $dend ?>')
+                    _el.append("<p class='text-center m-0'>Date: <?php echo date("F d, Y", strtotime($dstart)) ?></p>");
+                else
+                    _el.append("<p class='text-center m-0'>Date: <?php echo date("F d, Y", strtotime($dstart)) ?> - <?php echo date("F d, Y", strtotime($dend)) ?></p>");
+                if('<?php echo $rid ?>' > 0)
+                _el.append("<p class='text-center m-0'>For: <?php echo $rname ?></p>");
+                _el.append("<hr/>")
+                _el.append(_p)
+            var nw = window.open("","_blank","width=5000,heigth=5000,top=0,left=0")
+                nw.document.write(_el.html())
+                nw.document.close()
+                setTimeout(() => {
+                nw.print()
+                    setTimeout(() => {
+                        nw.close()
+                        DT_INIT()
+                        end_loader()
+                    }, 200);
+                }, 500);
+        })
+    })
 </script>
